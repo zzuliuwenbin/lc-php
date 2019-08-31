aa292392cfd8a58e23507bc0654315f99be33e0e
刘文斌 <zzuliuwenbin@126.com>
2019-08-31 11:33:01 +0800

    标准输入输出示例更新

--- input_output.php
+++ input_output.php
@@ -1,8 +1,8 @@
 <?php
 $a = "";
-$b = fgets(STDIN);//不会因为空格tab等空白字符而中断
+$b = fgets(STDIN);//不会因为空格tab等空白字符而中断,这个也会读入换行
 echo $b;
-$c = explode(" ", trim($b));
+$c = explode(" ", trim($b));//trim函数可以去除"\t"，"\n"，"\x0B" - 垂直制表符，"\r"，空格，当然也可以指定去除的字符
 var_dump($c);
 
 fscanf(STDIN,"%s",$a);//任何空白都会导致字符串输入中断
